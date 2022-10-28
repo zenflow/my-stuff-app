a429ec0ce41bd154aec048c7b11563068792a8ee
Matthew Francis Brunetti <zenflow87@gmail.com>
2022-10-28 19:07:09 -0400

    Initialize with create-next-app

---
+++ README.md
@@ -0,0 +1,11 @@
+# my-stuff-app
+Beautiful full-stack example app
+
+### Featuring
+
+- Next.js app dir
+- NextAuth.js
+- PostGraphile
+- Relay
+- React Admin
+- *and more*
---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>My Stuff</title>
+        <meta name="description" content="Beautiful full-stack example app"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
