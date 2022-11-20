d60b8f2276cfddfe4df642904f80e9bc7adb7da8
Matthew Francis Brunetti <zenflow87@gmail.com>
2022-11-20 06:52:27 -0500

    Add Apollo Client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import type { AppType } from "next/app";
+import { createWithApollo } from "next-ssr-with-apollo";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import Layout from "../components/Layout";
 import { createWithSession } from "../modules/next-ssr-with-next-auth";
 
@@ -16,4 +18,17 @@ export const withSession = createWithSession({
   refetchWhenOffline: false,
 });
 
-export default withSession(MyApp);
+const withApollo = createWithApollo({
+  client({ headers }) {
+    const isServer = typeof window === "undefined";
+    const baseUri = isServer ? process.env.SELF_URL : "";
+    const cookie = headers?.cookie;
+    return new ApolloClient({
+      uri: `${baseUri}/api/data/graphql`,
+      headers: cookie ? { cookie } : {},
+      cache: new InMemoryCache(),
+    });
+  },
+});
+
+export default withApollo(withSession(MyApp));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
+
+const HOME_PAGE_QUERY = gql`
+  query {
+    users {
+      nodes {
+        name
+        email
+        createdAt
+      }
+    }
+  }
+`;
+
 const HomePage: React.FC = () => {
+  const { error, data } = useQuery(HOME_PAGE_QUERY);
+  if (error) throw error;
   return (
     <>
       <h1>Home</h1>
+      <h2>Users</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 };
