8e8114c35b0da4ae43bd2d30889f2d395d4b9ff3
Matthew Francis Brunetti <zenflow87@gmail.com>
2023-04-23 22:27:04 -0400

    Add home page content

--- pages/index.route.tsx
+++ pages/index.route.tsx
@@ -1,9 +1,20 @@
+import Head from "next/head";
+import Link from "next/link";
 import type { MyPage } from "./_app.route";
+import { MAIN_TITLE } from "../common/constants";
 
 const HomePage: MyPage = () => {
   return (
     <>
-      <h1>Home</h1>
+      <Head>
+        <title>{`Home - ${MAIN_TITLE}`}</title>
+      </Head>
+      <div className="prose max-w-full text-center">
+        <h1>Home</h1>
+        <h3>
+          Check out <Link href="/memes">memes</Link>!
+        </h3>
+      </div>
     </>
   );
 };
